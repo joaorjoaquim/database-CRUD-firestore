b32906a0ac7f4a78b4e7ed99ba44f9732a61a4f1
Joao <joaorjoaquim@gmail.com>
2022-02-16 00:07:18 -0300

    creating actions

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 import { View, KeyboardAvoidingView, TextInput, TouchableOpacity, Text, StyleSheet, Animated, Keyboard } from 'react-native';
 import Routes from "./src/services/routes";
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App(){
-
-
-
-  useEffect(() => {
-        
-  },[])
-
   return(
-
-        <Routes/>
-
-
-  );
+    <Routes/>
+  )
 }
--- src/core/actions.js
+++ src/core/actions.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {} from 'react';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-const actions = () => {
-  return <View />;
+export default actions = () => {
+  const getFirebaseData = async (taskContext) => {
+    database.db.collection("Tasks").get()
+      .then((querySnapshot) => {
+          let list = []
+          querySnapshot.forEach(onSnapshot => {
+              let data = onSnapshot.data();
+              list.push({...data, id: onSnapshot.id}) //dados + document id
+          })
+          taskContext.list = list
+      })
+  }
 }
-
-export default actions;
