3586abdd2b5c1e5b773af1729f491cf0468a6205
Joao <joaorjoaquim@gmail.com>
2022-02-16 09:41:18 -0300

    Provider in the main js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, KeyboardAvoidingView, TextInput, TouchableOpacity, Text, StyleSheet, Animated, Keyboard } from 'react-native';
 import Routes from "./src/services/routes";
-import { NavigationContainer } from '@react-navigation/native';
+import AuthProvider from './src/services/auth'
 
 export default function App(){
   return(
-    <Routes/>
+    <AuthProvider>
+      <Routes/>
+    </AuthProvider>
   )
 }
